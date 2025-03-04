7eb3399db1b7ebb573265f0d4d9541474828cf4f
Aizhamal77 <damiraelayum@gmail.com>
2025-03-04 11:13:54 +0600

    Update and rename 7.cpp to структуры/7.cpp

--- структуры/7.cpp
+++ структуры/7.cpp
@@ -20,9 +20,9 @@ struct employee{
 
 void print_employee(const employee& emp){
 
-    cout << "Íîìåð: " << emp.employee_id << endl;
-    cout << "Çàðïëàòà: " << emp.salary << endl;
-    cout << "Äîëæíîñòü: ";
+    cout << "ÃÃ®Ã¬Ã¥Ã°: " << emp.employee_id << endl;
+    cout << "Ã‡Ã Ã°Ã¯Ã«Ã Ã²Ã : " << emp.salary << endl;
+    cout << "Ã„Ã®Ã«Ã¦Ã­Ã®Ã±Ã²Ã¼: ";
     switch (emp.position){
     case laborer: cout << "laborer"; break;
     case secretary: cout << "secretary"; break;
@@ -32,7 +32,7 @@ void print_employee(const employee& emp){
     case researcher: cout << "researcher"; break;
     }
     cout << endl;
-    cout << "Äàòà ïðèíÿòèÿ íà ðàáîòó: " << emp.hire_date.day << "/"
+    cout << "Ã„Ã Ã²Ã  Ã¯Ã°Ã¨Ã­Ã¿Ã²Ã¨Ã¿ Ã­Ã  Ã°Ã Ã¡Ã®Ã²Ã³: " << emp.hire_date.day << "/"
         << emp.hire_date.month << "/" << emp.hire_date.year << endl;
 }
 
@@ -41,13 +41,13 @@ int main(){
     employee employees[3];  
 
     for (int i = 0; i < 3; i++){
-        cout << "\nÑîòðóäíèê " << i + 1 << ":" << endl;
-        cout << "Ââåäèòå íîìåð ñîòðóäíèêà: ";
+        cout << "\nÃ‘Ã®Ã²Ã°Ã³Ã¤Ã­Ã¨Ãª " << i + 1 << ":" << endl;
+        cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã­Ã®Ã¬Ã¥Ã° Ã±Ã®Ã²Ã°Ã³Ã¤Ã­Ã¨ÃªÃ : ";
         cin >> employees[i].employee_id;
-        cout << "Ââåäèòå çàðïëàòó ñîòðóäíèêà: ";
+        cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã§Ã Ã°Ã¯Ã«Ã Ã²Ã³ Ã±Ã®Ã²Ã°Ã³Ã¤Ã­Ã¨ÃªÃ : ";
         cin >> employees[i].salary;
 
-        cout << "Ââåäèòå äîëæíîñòü (l - laborer, s - secretary, m - manager, a - accountant, e - executive, r - researcher): ";
+        cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¤Ã®Ã«Ã¦Ã­Ã®Ã±Ã²Ã¼ (l - laborer, s - secretary, m - manager, a - accountant, e - executive, r - researcher): ";
         char pos;
         cin >> pos;
 
@@ -58,16 +58,16 @@ int main(){
         case 'a': employees[i].position = accountant; break;
         case 'e': employees[i].position = executive; break;
         case 'r': employees[i].position = researcher; break;
-        default: cout << "Íåâåðíàÿ äîëæíîñòü!" << endl; return 1;
+        default: cout << "ÃÃ¥Ã¢Ã¥Ã°Ã­Ã Ã¿ Ã¤Ã®Ã«Ã¦Ã­Ã®Ã±Ã²Ã¼!" << endl; return 1;
         }
 
-        cout << "Ââåäèòå äàòó ïðèíÿòèÿ íà ðàáîòó (ÄÄ ÌÌ ÃÃÃÃ): ";
+        cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¤Ã Ã²Ã³ Ã¯Ã°Ã¨Ã­Ã¿Ã²Ã¨Ã¿ Ã­Ã  Ã°Ã Ã¡Ã®Ã²Ã³ (Ã„Ã„ ÃŒÃŒ ÃƒÃƒÃƒÃƒ): ";
         cin >> employees[i].hire_date.day >> employees[i].hire_date.month >> employees[i].hire_date.year;
     }
 
-    cout << "\nÈíôîðìàöèÿ î ñîòðóäíèêàõ:" << endl;
+    cout << "\nÃˆÃ­Ã´Ã®Ã°Ã¬Ã Ã¶Ã¨Ã¿ Ã® Ã±Ã®Ã²Ã°Ã³Ã¤Ã­Ã¨ÃªÃ Ãµ:" << endl;
     for (int i = 0; i < 3; i++) {
-        cout << "\nÑîòðóäíèê " << i + 1 << ":" << endl;
+        cout << "\nÃ‘Ã®Ã²Ã°Ã³Ã¤Ã­Ã¨Ãª " << i + 1 << ":" << endl;
         print_employee(employees[i]);
     }
 
