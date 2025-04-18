a8a5960069d6d6b1248545baf019de637be8c9f9
Aizhamal77 <damiraelayum@gmail.com>
2025-04-18 11:01:08 +0600

    Update and rename 2.cpp to Виртуальные функции/2.cpp

--- Виртуальные функции/2.cpp
+++ Виртуальные функции/2.cpp
@@ -10,15 +10,15 @@ protected:
 public:
     virtual void getdata() {
         cin.ignore();
-        cout << "\nÂâåäèòå çàãîëîâîê: ";
+        cout << "\nÃ‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã§Ã Ã£Ã®Ã«Ã®Ã¢Ã®Ãª: ";
         getline(cin, title);
-        cout << "Ââåäèòå öåíó: ";
+        cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¶Ã¥Ã­Ã³: ";
         cin >> price;
     }
 
     virtual void putdata() const {
-        cout << "\nÇàãîëîâîê: " << title;
-        cout << "\nÖåíà: " << price;
+        cout << "\nÃ‡Ã Ã£Ã®Ã«Ã®Ã¢Ã®Ãª: " << title;
+        cout << "\nÃ–Ã¥Ã­Ã : " << price;
     }
 
     virtual ~publication() {}
@@ -30,13 +30,13 @@ private:
 public:
     void getdata() override {
         publication::getdata();
-        cout << "Ââåäèòå ÷èñëî ñòðàíèö: ";
+        cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã·Ã¨Ã±Ã«Ã® Ã±Ã²Ã°Ã Ã­Ã¨Ã¶: ";
         cin >> pages;
     }
 
     void putdata() const override {
         publication::putdata();
-        cout << "\nÑòðàíèö: " << pages;
+        cout << "\nÃ‘Ã²Ã°Ã Ã­Ã¨Ã¶: " << pages;
     }
 };
 
@@ -46,13 +46,13 @@ private:
 public:
     void getdata() override {
         publication::getdata();
-        cout << "Ââåäèòå âðåìÿ çâó÷àíèÿ (â ìèíóòàõ): ";
+        cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¢Ã°Ã¥Ã¬Ã¿ Ã§Ã¢Ã³Ã·Ã Ã­Ã¨Ã¿ (Ã¢ Ã¬Ã¨Ã­Ã³Ã²Ã Ãµ): ";
         cin >> time;
     }
 
     void putdata() const override {
         publication::putdata();
-        cout << "\nÂðåìÿ çâó÷àíèÿ: " << time << " ìèíóò";
+        cout << "\nÃ‚Ã°Ã¥Ã¬Ã¿ Ã§Ã¢Ã³Ã·Ã Ã­Ã¨Ã¿: " << time << " Ã¬Ã¨Ã­Ã³Ã²";
     }
 };
 
@@ -64,7 +64,7 @@ public:
     void inputPublications() {
         char choice;
         do {
-            cout << "\nÂâîäèòü äàííûå äëÿ êíèãè èëè ïëåíêè (b/t)? ";
+            cout << "\nÃ‚Ã¢Ã®Ã¤Ã¨Ã²Ã¼ Ã¤Ã Ã­Ã­Ã»Ã¥ Ã¤Ã«Ã¿ ÃªÃ­Ã¨Ã£Ã¨ Ã¨Ã«Ã¨ Ã¯Ã«Ã¥Ã­ÃªÃ¨ (b/t)? ";
             cin >> choice;
 
             publication* pub = nullptr;
@@ -75,21 +75,21 @@ public:
                 pub = new tape;  
             }
             else {
-                cout << "Íåâåðíûé âûáîð. Ïîïðîáóéòå ñíîâà.\n";
+                cout << "ÃÃ¥Ã¢Ã¥Ã°Ã­Ã»Ã© Ã¢Ã»Ã¡Ã®Ã°. ÃÃ®Ã¯Ã°Ã®Ã¡Ã³Ã©Ã²Ã¥ Ã±Ã­Ã®Ã¢Ã .\n";
                 continue;
             }
 
             pub->getdata(); 
             pubList.push_back(pub);
 
-            cout << "Äîáàâèòü åù¸ îäíó ïóáëèêàöèþ? (y/n): ";
+            cout << "Ã„Ã®Ã¡Ã Ã¢Ã¨Ã²Ã¼ Ã¥Ã¹Â¸ Ã®Ã¤Ã­Ã³ Ã¯Ã³Ã¡Ã«Ã¨ÃªÃ Ã¶Ã¨Ã¾? (y/n): ";
             cin >> choice;
 
         } while ((choice == 'y' || choice == 'Y'));
     }
 
     void outputPublications() const {
-        cout << "\n--- Ñïèñîê ïóáëèêàöèé ---";
+        cout << "\n--- Ã‘Ã¯Ã¨Ã±Ã®Ãª Ã¯Ã³Ã¡Ã«Ã¨ÃªÃ Ã¶Ã¨Ã© ---";
         for (const auto& pub : pubList) {
             pub->putdata(); 
             cout << "\n-------------------------";
